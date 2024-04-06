7177e9c82ca2d4e412a196bcc6b1560229b2bcc6
Eamonn de Leastar <edeleastar@wit.ie>
2024-04-06 12:50:35 +0100

    include a layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<div class="container">
+  <div class="tabs notification is-primary">
+    <ul>
+      <li><a href="/">Start</a></li>
+      <li><a href="/login">Login</a></li>
+      <li><a href="/signup">Signup</a></li>
+    </ul>
+  </div>
+  <slot />
+</div>
