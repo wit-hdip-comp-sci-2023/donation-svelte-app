9f696008deceec144b5ffe988e30811c1d994516
Eamonn de Leastar <edeleastar@wit.ie>
2024-04-08 17:51:49 +0100

    first static version of donate form

--- src/routes/donate/+page.svelte
+++ src/routes/donate/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import { subTitle } from "$lib/stores";
+  import DonateForm from "./DonateForm.svelte";
 
   subTitle.set("Make a Donation");
 </script>
 
-<p>Make a donation here!</p>
+<DonateForm />
