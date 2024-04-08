b702ca6741c6d926d862b7b5b24423c1abe24558
Eamonn de Leastar <edeleastar@wit.ie>
2024-04-08 18:03:11 +0100

    use cards on report and donation

--- src/routes/report/+page.svelte
+++ src/routes/report/+page.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
+  import { subTitle } from "$lib/stores";
+  import Card from "$lib/ui/Card.svelte";
   import DonationList from "$lib/ui/DonationList.svelte";
+
+  subTitle.set("Donations to Date");
 </script>
 
-<DonationList />
+<Card title="Donations">
+  <DonationList />
+</Card>
