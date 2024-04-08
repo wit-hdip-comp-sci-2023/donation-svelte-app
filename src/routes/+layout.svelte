469318ce4659ec4de5cd8c25b8d03d3342f72e8b
Eamonn de Leastar <edeleastar@wit.ie>
2024-04-08 16:39:03 +0100

    starting screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,3 @@
 <div class="container">
-  <div class="tabs notification is-primary">
-    <ul>
-      <li><a href="/">Start</a></li>
-      <li><a href="/login">Login</a></li>
-      <li><a href="/signup">Signup</a></li>
-    </ul>
-  </div>
   <slot />
 </div>
