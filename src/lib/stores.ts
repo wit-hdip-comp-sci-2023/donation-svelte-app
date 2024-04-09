ef90b0c5b6f44ee6f05e3994971d8b51e2ef8907
Eamonn de Leastar <edeleastar@wit.ie>
2024-04-09 17:38:52 +0100

    new store to hold latest donation

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,5 +1,6 @@
 import { writable } from "svelte/store";
-import type { Session } from "$lib/types/donation-types";
+import type { Donation, Session } from "$lib/types/donation-types";
 
 export const currentSession = writable<Session>();
 export const subTitle = writable<string>();
+export const latestDonation = writable<Donation>();
