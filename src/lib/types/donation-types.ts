96c0320bdef0b1480d9be294a9c0d5c773977403
Eamonn de Leastar <edeleastar@wit.ie>
2024-04-09 12:44:03 +0100

    introduce session and user types

---
+++ src/lib/types/donation-types.ts
@@ -0,0 +1,13 @@
+export interface Session {
+  name: string;
+  _id: string;
+  token: string;
+}
+
+export interface User {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  _id?: string;
+}
