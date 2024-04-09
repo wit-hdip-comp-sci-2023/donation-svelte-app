11cc97526e735cdc3bd8d3c0e093104b2c5f8f41
Eamonn de Leastar <edeleastar@wit.ie>
2024-04-09 13:14:55 +0100

    support donate in donationService

--- src/lib/services/donation-service.ts
+++ src/lib/services/donation-service.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import type { Session, User } from "$lib/types/donation-types";
+import type { Candidate, Donation } from "$lib/types/donation-types";
 
 export const donationService = {
   baseUrl: "http://localhost:4000",
@@ -30,6 +31,36 @@ export const donationService = {
     } catch (error) {
       console.log(error);
       return null;
+    }
+  },
+
+  async donate(donation: Donation, session: Session) {
+    try {
+      axios.defaults.headers.common["Authorization"] = "Bearer " + session.token;
+      const response = await axios.post(this.baseUrl + "/api/candidates/" + donation.candidate + "/donations", donation);
+      return response.status == 200;
+    } catch (error) {
+      return false;
+    }
+  },
+
+  async getCandidates(session: Session): Promise<Candidate[]> {
+    try {
+      axios.defaults.headers.common["Authorization"] = "Bearer " + session.token;
+      const response = await axios.get(this.baseUrl + "/api/candidates");
+      return response.data;
+    } catch (error) {
+      return [];
+    }
+  },
+
+  async getDonations(session: Session): Promise<Donation[]> {
+    try {
+      axios.defaults.headers.common["Authorization"] = "Bearer " + session.token;
+      const response = await axios.get(this.baseUrl + "/api/donations");
+      return response.data;
+    } catch (error) {
+      return [];
     }
   }
 };
