64fe4a8c796f34956a10ee941020923834536d8c
Eamonn de Leastar <edeleastar@wit.ie>
2024-04-09 16:52:57 +0100

    recover the session (if available) as the layout is reloaded

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
+  import { browser } from "$app/environment";
   import { currentSession } from "$lib/stores";
   import Heading from "$lib/ui/Heading.svelte";
   import Menu from "$lib/ui/Menu.svelte";
+
+  if (browser) {
+    const savedSession = localStorage.donation;
+    if (savedSession) {
+      const session = JSON.parse(savedSession);
+      currentSession.set(session);
+    }
+  }
 </script>
 
 <div class="container">
