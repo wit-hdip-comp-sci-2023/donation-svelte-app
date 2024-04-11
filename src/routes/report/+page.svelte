b76395cd9deb786effaaf2bbdc107ac57b415446
Eamonn de Leastar <edeleastar@wit.ie>
2024-04-11 15:16:24 +0100

    refactor report to us a server component

--- src/routes/report/+page.svelte
+++ src/routes/report/+page.svelte
@@ -1,19 +1,13 @@
 <script lang="ts">
-  import { donationService } from "$lib/services/donation-service";
-  import type { Donation } from "$lib/types/donation-types";
-  import { currentSession, subTitle } from "$lib/stores";
+  import { subTitle } from "$lib/stores";
   import Card from "$lib/ui/Card.svelte";
   import DonationList from "$lib/ui/DonationList.svelte";
-  import { onMount } from "svelte";
-  import { get } from "svelte/store";
+
+  export let data: any;
 
   subTitle.set("Donations to Date");
-  let donations: Donation[] = [];
-  onMount(async () => {
-    donations = await donationService.getDonations(get(currentSession));
-  });
 </script>
 
 <Card title="Donations">
-  <DonationList {donations} />
+  <DonationList donations={data.donations} />
 </Card>
