557e72c3a9a0ae629cd1f9556a77f65e2cfaf41c
Eamonn de Leastar <edeleastar@wit.ie>
2024-04-11 16:09:14 +0100

    convert charts to ssr

--- src/routes/charts/+page.svelte
+++ src/routes/charts/+page.svelte
@@ -1,37 +1,22 @@
 <script lang="ts">
   // @ts-ignore
   import Chart from "svelte-frappe-charts";
-  import { onMount } from "svelte";
-  import { donationService } from "$lib/services/donation-service";
-  import { currentSession, subTitle } from "$lib/stores";
-  import { get } from "svelte/store";
   import Card from "$lib/ui/Card.svelte";
-  import type { DataSet } from "$lib/types/donation-types";
-  import { generateByMethod, generateByCandidate } from "$lib/services/donation-utils";
-
-  let totalByMethod: DataSet;
-  let donationsByCandidate: DataSet;
+  import { subTitle } from "$lib/stores";
+  export let data: any;
 
   subTitle.set("Donations Data");
-
-  onMount(async () => {
-    const donationList = await donationService.getDonations(get(currentSession));
-    const candidates = await donationService.getCandidates(get(currentSession));
-
-    totalByMethod = generateByMethod(donationList);
-    donationsByCandidate = generateByCandidate(donationList, candidates);
-  });
 </script>
 
 <div class="columns">
   <div class="column">
     <Card title="Donations By Method">
-      <Chart data={totalByMethod} type="bar" />
+      <Chart data={data.byMethod} type="bar" />
     </Card>
   </div>
-  <div class="column has-text-centered">
-    <Card title="Donations By Method">
-      <Chart data={donationsByCandidate} type="pie" />
+  <div class="column">
+    <Card title="Donations By Candidate">
+      <Chart data={data.byCandidate} type="pie" />
     </Card>
   </div>
 </div>
