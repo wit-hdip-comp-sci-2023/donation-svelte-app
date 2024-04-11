85708671a145adc7eac5397a5e0f0bae92454d6a
Eamonn de Leastar <edeleastar@wit.ie>
2024-04-11 16:02:50 +0100

    convert DonateForm to ssr

--- src/routes/donate/+page.server.ts
+++ src/routes/donate/+page.server.ts
@@ -12,3 +12,26 @@ export const load: PageServerLoad = async ({ cookies }) => {
     };
   }
 };
+
+export const actions = {
+  donate: async ({ request, cookies }) => {
+    const donationUser = cookies.get("donation-user") as string;
+    if (donationUser) {
+      console.log(donationUser);
+      const user = JSON.parse(donationUser) as Session;
+      const form = await request.formData();
+      console.log(form);
+      const donation = {
+        amount: form.get("amount") as unknown as number,
+        method: form.get("method") as string,
+        candidate: form.get("candidate") as string,
+        lat: form.get("lat") as unknown as number,
+        lng: form.get("lng") as unknown as number,
+        donor: user._id,
+        _id: ""
+      };
+      console.log(donation);
+      donationService.donate(donation, user);
+    }
+  }
+};
