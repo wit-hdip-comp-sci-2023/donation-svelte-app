83ab260c9c140892926c0f54c554c2aa89e83cdd
Eamonn de Leastar <edeleastar@wit.ie>
2024-04-11 15:59:16 +0100

    convert form to use server

--- src/routes/donate/DonateForm.svelte
+++ src/routes/donate/DonateForm.svelte
@@ -1,67 +1,35 @@
 <script lang="ts">
-  import { donationService } from "$lib/services/donation-service";
-  import type { Candidate, Donation } from "$lib/types/donation-types";
-  import { currentSession, latestDonation } from "$lib/stores";
+  import { enhance } from "$app/forms";
+  import type { Candidate } from "$lib/types/donation-types";
   import Coordinates from "$lib/ui/Coordinates.svelte";
-  import { get } from "svelte/store";
 
   export let candidateList: Candidate[] = [];
 
-  let amount = 0;
   let lat = 52.160858;
   let lng = -7.15242;
-  let selectedCandidate = "";
   let paymentMethods = ["paypal", "direct"];
-  let selectedMethod = "";
   let message = "Please donate";
-
-  async function donate() {
-    if (selectedCandidate && amount && selectedMethod) {
-      const candidate = candidateList.find((candidate) => candidate._id === selectedCandidate);
-      if (candidate) {
-        const donation: Donation = {
-          amount: amount,
-          method: selectedMethod,
-          candidate: selectedCandidate,
-          lat: lat,
-          lng: lng,
-          donor: $currentSession._id
-        };
-        const success = await donationService.donate(donation, get(currentSession));
-        if (!success) {
-          message = "Donation not completed - some error occurred";
-          return;
-        }
-        donation.candidate = candidate;
-        donation.donor = $currentSession.name;
-        latestDonation.set(donation);
-        message = `Thanks! You donated ${amount} to ${candidate.firstName} ${candidate.lastName}`;
-      }
-    } else {
-      message = "Please select amount, method and candidate";
-    }
-  }
 </script>
 
-<form on:submit|preventDefault={donate}>
+<form method="POST" action="?/donate" use:enhance>
   <div class="field">
     <label class="label" for="amount">Enter Amount:</label>
-    <input bind:value={amount} class="input" id="amount" name="amount" type="number" />
+    <input class="input" id="amount" name="amount" type="number" />
   </div>
   <div class="field">
     <div class="control">
       <label class="label" for="amount">Select Payment Method:</label>
       {#each paymentMethods as method}
-        <input bind:group={selectedMethod} class="radio" type="radio" value={method} /> {method}
+        <input class="radio" type="radio" value={method} name="method" /> {method}
       {/each}
     </div>
   </div>
   <div class="field">
     <label class="label" for="amount">Select Candidate:</label>
     <div class="select">
-      <select bind:value={selectedCandidate}>
+      <select name="candidate">
         {#each candidateList as candidate}
-          <option value={candidate._id}>{candidate.lastName},{candidate.firstName}</option>
+          <option value={candidate._id}>{candidate.lastName},{candidate.firstName} </option>
         {/each}
       </select>
     </div>
